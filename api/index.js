e51e1f9f6a89a0f00b3fe00d41b4c128dc8f3ea1
Himanshu Mishra <himanshu404mishra@gmail.com>
2025-03-02 20:03:24 +0545

    problem in public filder fixed

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({extended: true}))
 app.use("/", indexRouter)
 app.use("/user", userRouter)
 
-app.use(express.static("public"))
+app.use(express.static(path.join(__dirname, '../public')))
 
 
 
